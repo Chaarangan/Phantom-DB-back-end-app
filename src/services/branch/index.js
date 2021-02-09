c17684fccd3fa67ce41e9fdec17360e34acd18a6
Chaarangan <chaanuv@gmail.com>
2021-02-09 16:36:44 +0530

    raw_query

--- src/services/branch/index.js
+++ src/services/branch/index.js
@@ -5,7 +5,7 @@ const { QueryTypes } = require('sequelize');
 
 const getBranches = async(req, res, next) => {
     try {
-        const branches = await sequelize.query("SELECT * FROM `branches`", { type: QueryTypes.SELECT });
+        const branches = await sequelize.query("SELECT * FROM `branches`");
         req.branches = branches;
         next();
     } catch (e) {
