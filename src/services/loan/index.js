87aec597b34cdd0e2ae7e65dbb8ac1090beb936e
Chaarangan <chaanuv@gmail.com>
2021-03-03 23:27:52 +0530

    ok

--- src/services/loan/index.js
+++ src/services/loan/index.js
@@ -4,70 +4,73 @@ const now = new Date();
 
 const approveLoanRequest = async (req, res, next) => {
     try {
-        const   loan_id = req.params.loan_id,
-                approved_by = req.user.employee_id;
+        const loan_id = req.params.loan_id,
+            approved_by = req.user.employee_id;
         await sequelize.query("CALL approveLoan(" + loan_id + "," + approved_by + ")").then(
             async (results) => {
-                if(results[0].OK == "OK"){              
+                if (results[0].OK == "OK") {
                     req.message = "Approved Successfully!";
-                    next();                    
+                    next();
                 }
                 else {
-                    return res.status(404).json({ response: "Failed!", status : 404});
+                    return res.status(404).json({ response: "Failed!", status: 404 });
                 }
             }
         );
     } catch (e) {
         console.log(e);
-        return res.status(400).json({status: 400, response: "Bad Request!"});
+        return res.status(400).json({ status: 400, response: "Bad Request!" });
     }
 };
 
 
 const rejectLoanRequest = async (req, res, next) => {
     try {
-        const   
-                loan_id = req.params.loan_id,
-                rejected_by = req.user.employee_id;
-                
+        const
+            loan_id = req.params.loan_id,
+            rejected_by = req.user.employee_id;
+
         await sequelize.query("CALL rejectLoan(" + loan_id + "," + rejected_by + ")").then(
             async (results) => {
-                if(results[0].OK == "OK"){                        
+                if (results[0].OK == "OK") {
                     req.message = "Rejected Successfully!";
                     next();
                 }
                 else {
-                    return res.status(400).json({ response: "Failed!", status : 400 });
+                    return res.status(400).json({ response: "Failed!", status: 400 });
                 }
             }
         );
     } catch (e) {
         console.log(e);
-        return res.status(400).json({status: 400, response: "Failed!. Try again."});
+        return res.status(400).json({ status: 400, response: "Failed!. Try again." });
     }
 };
 
-
-
 const createLoanRequest = async (req, res, next) => {
     try {
-        const   loan_type = req.body.loan_type,
-                account_no = req.body.account_no,
-                amount = req.body.amount,
-                branch_id = req.user.branch_id,
-                time_period = req.body.time_period,
-                request_date = date.format(now, 'YYYY-MM-DD HH:mm:ss GMT+0530'),
-                request_by = req.user.employee_id;
-                
-        await sequelize.query("INSERT INTO loans SET loan_type = ?, account_no = ?, amount = ?, branch_id = ?, time_period = ?, installment = calculateInstallment(" + amount + "," + loan_type + "," + time_period + "), requested_date = ?, request_by = ?, loan_status = ?", {replacements : [loan_type, account_no, amount, branch_id, time_period, request_date, request_by, 0]}).then(
-            async (results) => {    
-                req.message = "Success";
-                next();
-            }
-        );
+        const loan_type = req.body.loan_type,
+            account_no = req.body.account_no,
+            amount = req.body.amount,
+            branch_id = req.user.branch_id,
+            time_period = req.body.time_period,
+            request_date = date.format(now, 'YYYY-MM-DD HH:mm:ss GMT+0530'),            
+            requested_by = req.user.employee_id;
+
+        await sequelize.query("SELECT calculateInstallment(?,?,?) as installment", { replacements: [amount, loan_type, time_period] }).then(
+            async (foundInstallment) => {
+
+                const installment = foundInstallment[0][0].installment;
+                await sequelize.query("INSERT INTO loans SET loan_type = ?, account_no = ?, amount = ?, branch_id = ?, time_period = ?, installment = ?, requested_date = ?, requested_by = ?, loan_status = ?", { replacements: [loan_type, account_no, amount, branch_id, time_period, installment, request_date, requested_by, 0] }).then(
+                    async (results) => {
+                        req.message = "Success";
+                        next();
+                    }
+                );
+            });
     } catch (e) {
         console.log(e);
-        return res.status(400).json({status: 400, response: "Bad Request!"});
+        return res.status(400).json({ status: 400, response: "Bad Request!" });
     }
 };
 
@@ -80,13 +83,13 @@ const getRequestedLoans = async (req, res, next) => {
                     next();
                 }
                 else {
-                    return res.status(404).json({ response: "No Loan Requests found!", status : 404 });
+                    return res.status(404).json({ response: "No Loan Requests found!", status: 404 });
                 }
             }
         );
     } catch (e) {
         console.log(e);
-        return res.status(400).json({status: 400, response: "Bad Request!"});
+        return res.status(400).json({ status: 400, response: "Bad Request!" });
     }
 };
 
@@ -99,13 +102,13 @@ const getLoans = async (req, res, next) => {
                     next();
                 }
                 else {
-                    return res.status(404).json({ response: "No Loans found!", status : 404 });
+                    return res.status(404).json({ response: "No Loans found!", status: 404 });
                 }
             }
         );
     } catch (e) {
         console.log(e);
-        return res.status(400).json({status: 400, response: "Bad Request!"});
+        return res.status(400).json({ status: 400, response: "Bad Request!" });
     }
 };
 
@@ -121,13 +124,13 @@ const getBankVisitLoans = async (req, res, next) => {
                     next();
                 }
                 else {
-                    return res.status(404).json({ response: "No Loans found!", status : 404 });
+                    return res.status(404).json({ response: "No Loans found!", status: 404 });
                 }
             }
         );
     } catch (e) {
         console.log(e);
-        return res.status(400).json({status: 400, response: "Bad Request!"});
+        return res.status(400).json({ status: 400, response: "Bad Request!" });
     }
 };
 
@@ -143,13 +146,13 @@ const getOnlineLoans = async (req, res, next) => {
                     next();
                 }
                 else {
-                    return res.status(404).json({ response: "No Online Loans found!", status : 404 });
+                    return res.status(404).json({ response: "No Online Loans found!", status: 404 });
                 }
             }
         );
     } catch (e) {
         console.log(e);
-        return res.status(400).json({status: 400, response: "Bad Request!"});
+        return res.status(400).json({ status: 400, response: "Bad Request!" });
     }
 };
 
@@ -158,21 +161,21 @@ const getOnlineLoans = async (req, res, next) => {
 
 
 const getRejectedLoans = async (req, res, next) => {
-    try {                      
+    try {
         await sequelize.query("SELECT loan_id, getLoanType(loan_type) as loan_type, account_no, amount, getBranch(branch_id) as branch_name, time_period, installment, requested_date, getEmployeeName(requested_by) as requested_by, getLoanStatus(loan_status) as loan_status  FROM loans WHERE loan_status = 3 ORDER BY loan_id ASC").then(
             async (foundLoans) => {
-                if (foundLoans[0].length != 0) {                    
+                if (foundLoans[0].length != 0) {
                     req.loans = foundLoans[0];
                     next();
                 }
                 else {
-                    return res.status(404).json({ response: "No Rejected Loans found!", status : 404 });
+                    return res.status(404).json({ response: "No Rejected Loans found!", status: 404 });
                 }
             }
         );
     } catch (e) {
         console.log(e);
-        return res.status(400).json({status: 400, response: "Failed!. Try again."});
+        return res.status(400).json({ status: 400, response: "Failed!. Try again." });
     }
 };
 
