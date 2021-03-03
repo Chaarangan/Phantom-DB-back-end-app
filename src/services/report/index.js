9ed738594b503e33e06a854f9cf37d9ef35b44fb
Chaarangan <chaanuv@gmail.com>
2021-03-04 03:11:26 +0530

    ok

--- src/services/report/index.js
+++ src/services/report/index.js
@@ -19,7 +19,7 @@ const transaction_report = async (req, res, next) => {
 const loan_reports = async (req, res, next) => {
     const branch_id = req.user.branch_id;
     try {
-        await sequelize.query("SELECT installment_id, loan_id, amount, due_date, paid_date, getInstallmentStatus(installment_status) FROM loan_installment_banks left join loans l using(loan_id) WHERE l.branch_id = ?", { replacements: [branch_id] }).then(
+        await sequelize.query("SELECT lib.installment_id, lib.loan_id, l.amount, lib.due_date, lib.paid_date, getInstallmentStatus(lib.installment_status) as status FROM loan_installment_banks lib left join loans l using(loan_id) WHERE l.branch_id = ?", { replacements: [branch_id] }).then(
             async (foundInstallments) => {
                 if (foundInstallments[0].length != 0) {
                     req.foundInstallments = foundInstallments;
@@ -35,28 +35,5 @@ const loan_reports = async (req, res, next) => {
         return res.status(400).json({ status: 400, response: "Bad Request!" });
     }
 }
-
-const loan_reportss = async (req, res, next) => {
-    const   branch_id = req.user.branch_id,
-            current_month = date.format(new Date(), 'MM'); 
-
-    try {
-        await sequelize.query("SELECT installment_id, loan_id, amount, due_date, paid_date, installment_status  FROM loan_installment_banks left join loans l using(loan_id) WHERE l.branch_id = ?", { replacements: [branch_id] }).then(
-            async (foundInstallments) => {
-                if (foundInstallments[0].length != 0) {
-                    req.foundInstallments = foundInstallments;
-                    next();
-                }
-                else {
-                    return res.status(404).json({ response: "No Installments found!", status: 404 });
-                }
-            }
-        );
-    } catch (e) {
-        console.log(e);
-        return res.status(400).json({ status: 400, response: "Bad Request!" });
-    }
-}
-
 
 module.exports = { transaction_report, loan_reports }
