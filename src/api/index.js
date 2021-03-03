7136d074c5b2fd8eb836c8664b6152494cd83b1f
Chaarangan <chaanuv@gmail.com>
2021-03-03 19:44:47 +0530

    ok

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,8 @@
-const adminRoutes = require("./admin");
 const managerRoutes = require("./manager");
 const clerkRoutes = require("./clerk");
 const customerRoutes = require("./customer");
 
 const endPointsHandler = (app) => {
-  app.use("/api/admin/", adminRoutes);
   app.use("/api/manager/", managerRoutes);
   app.use("/api/clerk/", clerkRoutes);
   app.use("/api/customer/", customerRoutes);
